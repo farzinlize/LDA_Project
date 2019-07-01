7d4a2d6473b11ccc6c0d2b1619efaf2ff802bc24
Farzin <farzinlize@live.com>
2019-07-01 16:54:58 +0430

    extraxt topics for report added

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
 import gensim
 
+def extraxtWords(topicStr):
+    lst = topicStr.split('"')
+    result = ""
+    for i in range(len(lst)):
+        if i%2 == 1:
+            result += lst[i] + ", "
+    return result[:-2]
+
 dataset = open("dataset.txt", "r")
 
 texts = []
@@ -26,4 +34,5 @@ corpus = [dictionary.doc2bow(text) for text in texts]
 # generate LDA model
 ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=10, id2word = dictionary, passes=20)
 
-ldamodel.show_topics()
+for topic in ldamodel.show_topics(num_topics=10, num_words=10):
+    print(extraxtWords(topic[1]))
