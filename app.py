4f144b8a707f32060511cc97d488ff3e65ec1aca
Farzin <farzinlize@live.com>
2019-07-02 22:40:09 +0430

    complite queries and mesaure success or fail variables

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import gensim
 
 dataset = open("dataset.txt", "r")
 
-num_topics = 10
+num_topics = 8
 
 texts = []
 
@@ -45,17 +45,36 @@ dataset_test = open("dataset-test.txt", "r")
 result_file = open("result.txt", "w")
 dataset_test.readline()
 
-success = 0
-fail = 0
-tests_num = 0
-varians = 0
+success_1 = 0   # positive emotion value for query in the most related topic
+success_2 = 0   # positive emotion value for query in the second most related topic
+fail = 0        # zero value for query at two first topic
+varians = 0     # avrage different between positive success values
+
+accept_threshold = 0.0001
 
 for line in dataset_test:
     terms = line.split()
-    best_macth = query(terms[0], ldamodel, dictionary)[0]
-    related_emotion_score = emotions[terms[-1]][best_macth[0]]
-    # print(terms[0], float(terms[1]), related_emotion_score)
-    result_file.write(terms[0] + ":" + terms[1] + " --> " + str(related_emotion_score) + "\n")
+    if float(terms[1]) < 0.5:
+        continue
+    macths_list = query(terms[0], ldamodel, dictionary)
+    first_related_emotion_score = emotions[terms[-1]][macths_list[0][0]]
+    second_related_emotion_score = emotions[terms[-1]][macths_list[1][0]]
+    if first_related_emotion_score > accept_threshold:
+        success_1 += 1
+        varians += float(terms[1]) - first_related_emotion_score
+    elif second_related_emotion_score > accept_threshold:
+        success_2 += 1
+        varians += float(terms[1]) - second_related_emotion_score
+    else:
+        fail += 1
+    result_file.write(str(macths_list[0][1]) + ", " + str(macths_list[1][1]) + "\n")
 
-# query_result = query('hatred', ldamodel, dictionary)
-# print(query_result)
+varians = varians / (success_1 + success_2)
+
+dataset_test.close()
+result_file.close()
+
+print("positive emotion value for query in the most related topic: ", success_1)
+print("positive emotion value for query in the second most related topic: ", success_2)
+print("zero value for query at two first topic: ", fail)
+print("avrage different between positive success values: ", varians)
